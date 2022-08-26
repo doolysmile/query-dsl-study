e1f48cd67d8865984bcfeb29b0a666263ec56c51
KCH <doolysmile1@naver.com>
2022-08-26 14:13:14 +0900

    feat : user엔티티 추가

---
+++ src/main/java/com/ll/exam/qsl/user/entity/SiteUser.java
@@ -0,0 +1,27 @@
+package com.ll.exam.qsl.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
