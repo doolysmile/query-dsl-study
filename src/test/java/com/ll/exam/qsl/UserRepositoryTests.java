7f29771f6290ecff1fbe4a24f6ec3f9487d2c7f6
KCH <doolysmile1@naver.com>
2022-08-29 13:08:21 +0900

    test : 테스트 케이스 추가

--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -1,5 +1,3 @@
-// UserRepositoryTests.java
-
 package com.ll.exam.qsl;
 
 import com.ll.exam.qsl.user.entity.SiteUser;
@@ -28,13 +26,24 @@ class UserRepositoryTests {
     }
 
     @Test
-    @DisplayName("1번 회원 찾기")
+    @DisplayName("1번 회원을 Qsl로 가져오기")
     void t2() {
         SiteUser u1 = userRepository.getQslUser(1L);
 
         assertThat(u1.getId()).isEqualTo(1L);
         assertThat(u1.getUsername()).isEqualTo("user1");
-        assertThat(u1.getPassword()).isEqualTo("{noop}1234");
         assertThat(u1.getEmail()).isEqualTo("user1@test.com");
+        assertThat(u1.getPassword()).isEqualTo("{noop}1234");
+    }
+
+    @Test
+    @DisplayName("2번 회원을 Qsl로 가져오기")
+    void t3() {
+        SiteUser u2 = userRepository.getQslUser(2L);
+
+        assertThat(u2.getId()).isEqualTo(2L);
+        assertThat(u2.getUsername()).isEqualTo("user2");
+        assertThat(u2.getEmail()).isEqualTo("user2@test.com");
+        assertThat(u2.getPassword()).isEqualTo("{noop}1234");
     }
 }
