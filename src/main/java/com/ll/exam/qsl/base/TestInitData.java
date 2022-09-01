de72302e89836b4beea184c5109c05fe820fa01d
KCH <doolysmile1@naver.com>
2022-09-01 13:02:29 +0900

    test : 1번 회원 2번 회원 모두 농구에 관심이 있을 때

--- src/main/java/com/ll/exam/qsl/base/TestInitData.java
+++ src/main/java/com/ll/exam/qsl/base/TestInitData.java
@@ -29,15 +29,16 @@ public class TestInitData {
                     .email("user2@test.com")
                     .build();
 
+            userRepository.saveAll(Arrays.asList(u1, u2));
+
             u1.addInterestKeywordContent("축구");
             u1.addInterestKeywordContent("농구");
 
             u2.addInterestKeywordContent("클라이밍");
             u2.addInterestKeywordContent("마라톤");
+            u2.addInterestKeywordContent("농구");
 
             userRepository.saveAll(Arrays.asList(u1, u2));
-
-            List<SiteUser> siteUsers = userRepository.saveAll(Arrays.asList(u1, u2));
         };
     }
 }
