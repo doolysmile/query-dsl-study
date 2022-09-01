cd12edff1d8dea41e2fba4b1d23763e24ab4838d
KCH <doolysmile1@naver.com>
2022-09-01 13:01:46 +0900

    test : 축구에 관심 있는 회원 검색

--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -198,12 +198,26 @@ class UserRepositoryTests {
         // interest_keyword 테이블에 축구, 롤, 헬스에 해당하는 row 3개 생성
     }
     @Test
-    @DisplayName("축구에 관심이 있는 회원을 검색할 수 있다.")
+    @DisplayName("QSL : 축구에 관심이 있는 회원을 검색할 수 있다.")
     void t11() {
 
         // 테스트 케이스 추가
         // 구현, QueryDSL 사용
         List<SiteUser> users = userRepository.getQslUserByInterestKeyword("축구");
+
+        SiteUser u = users.get(0);
+        assertThat(u.getId()).isEqualTo(1L);
+        assertThat(u.getUsername()).isEqualTo("user1");
+        assertThat(u.getEmail()).isEqualTo("user1@test.com");
+        assertThat(u.getPassword()).isEqualTo("{noop}1234");
+    }
+    @Test
+    @DisplayName("Spring Data JPA : 축구에 관심이 있는 회원을 검색할 수 있다.")
+    void t12() {
+
+        // 테스트 케이스 추가
+        // 구현, QueryDSL 사용
+        List<SiteUser> users = userRepository.findByInterestKeywords_content("축구");
 
         SiteUser u = users.get(0);
         assertThat(u.getId()).isEqualTo(1L);
